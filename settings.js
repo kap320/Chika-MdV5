1276e737ce738d9af8f6713ff4c8068346d0271f
kap320 <77118749+kap320@users.noreply.github.com>
2022-04-28 16:52:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,16 +18,16 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281575886399','6281575886399','6281575886399']
-global.premium = ['6281575886399']
-global.ownernomer = '6281575886399'
-global.ownername = 'RiychDwayne'
-global.botname = 'Chika-Md'
-global.footer = 'RiychDwayne'
-global.ig = 'https://instagram.com/riychdwayne'
-global.email = 'riychh.20@gmail.com'
+global.owner = ['6281391883354','6281391883354','6281391883354']
+global.premium = ['6281391883354']
+global.ownernomer = '6281391883354'
+global.ownername = 'NakanoMiku'
+global.botname = 'NakanoMiku「MD」'
+global.footer = 'NakanoMiku「MD」'
+global.ig = 'https://instagram.com/anime.purwokerto'
+global.email = 'boyahmad13131@gmail.com'
 global.region = 'Indonesia'
-global.sc = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
+global.sc = 'https://Bit.ly/3qCgkKv'
 global.myweb = 'https://api-riychdwayne.herokuap.com'
 global.packname = 'Chika-Md'
 global.author = 'WhatsApp Bot'
